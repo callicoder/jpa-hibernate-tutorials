6ab17a8c63ca5790b0fb032e83fbb595385ab246
Amit Patil <gpamitranjit@gmail.com>
2019-09-29 23:43:34 +0530

    portable way to generate the identifier for managed entity with either sequene generator or identity generator( if the database does not support former one)

--- jpa-one-to-many-demo/src/main/java/com/amit/jpa/model/Comment.java
+++ jpa-one-to-many-demo/src/main/java/com/amit/jpa/model/Comment.java
@@ -7,51 +7,53 @@ import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+
 /**
-*
-* @author Amit Patil
-*
-**/
+ *
+ * @author Amit Patil
+ *
+ **/
 @Entity
 @Table(name = "comments")
 public class Comment extends AuditModel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceComment")
+	@SequenceGenerator(name = "sequenceComment", allocationSize = 10)
+	private Long id;
 
-    @NotNull
-    @Lob
-    private String text;
+	@NotNull
+	@Lob
+	private String text;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "post_id", nullable = false)
-    @OnDelete(action = OnDeleteAction.CASCADE)
-    @JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
-    @JsonIdentityReference(alwaysAsId=true)
-    @JsonProperty("post_id")
-    private Post post;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "post_id", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+	@JsonIdentityReference(alwaysAsId = true)
+	@JsonProperty("post_id")
+	private Post post;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getText() {
-        return text;
-    }
+	public String getText() {
+		return text;
+	}
 
-    public void setText(String text) {
-        this.text = text;
-    }
+	public void setText(String text) {
+		this.text = text;
+	}
 
-    public Post getPost() {
-        return post;
-    }
+	public Post getPost() {
+		return post;
+	}
 
-    public void setPost(Post post) {
-        this.post = post;
-    }
+	public void setPost(Post post) {
+		this.post = post;
+	}
 }
--- jpa-one-to-many-demo/src/main/java/com/amit/jpa/model/Post.java
+++ jpa-one-to-many-demo/src/main/java/com/amit/jpa/model/Post.java
@@ -3,61 +3,63 @@ package com.amit.jpa.model;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
 /**
-*
-* @author Amit Patil
-*
-**/
+ *
+ * @author Amit Patil
+ *
+ **/
 @Entity
 @Table(name = "posts")
 public class Post extends AuditModel {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequencePost")
+	@SequenceGenerator(name = "sequencePost", allocationSize = 10)
+	private Long id;
 
-    @NotNull
-    @Size(max = 100)
-    @Column(unique = true)
-    private String title;
+	@NotNull
+	@Size(max = 100)
+	@Column(unique = true)
+	private String title;
 
-    @NotNull
-    @Size(max = 250)
-    private String description;
+	@NotNull
+	@Size(max = 250)
+	private String description;
 
-    @NotNull
-    @Lob
+	@NotNull
+	@Lob
 //    @Column(name = "content", nullable = false, columnDefinition = "CLOB")
-    private String content;
+	private String content;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	public String getContent() {
+		return content;
+	}
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
