3a2c693b0052c2b806ec639d696c94b985f3bba5
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-10-20 21:50:23 +0200

    _refactor

--- jpa-one-to-one-demo/src/main/java/com/example/jpa/JpaOneToOneDemoApplication.java
+++ jpa-one-to-one-demo/src/main/java/com/example/jpa/JpaOneToOneDemoApplication.java
@@ -1,61 +1,41 @@
 package com.example.jpa;
 
-import com.example.jpa.model.Gender;
 import com.example.jpa.model.User;
 import com.example.jpa.model.UserProfile;
-import com.example.jpa.repository.UserRepository;
 import com.example.jpa.repository.UserProfileRepository;
+import com.example.jpa.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.util.Calendar;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+import java.util.List;
 
 @SpringBootApplication
-public class JpaOneToOneDemoApplication implements CommandLineRunner {
+@RestController
+public class JpaOneToOneDemoApplication {
+
 
 	@Autowired
 	private UserRepository userRepository;
 
 	@Autowired
-    private UserProfileRepository userProfileRepository;
+	private UserProfileRepository userProfileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaOneToOneDemoApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// Clean up database tables
-		userProfileRepository.deleteAllInBatch();
-		userRepository.deleteAllInBatch();
-
-		//=========================================
-
-		// Create a User instance
-		User user = new User("Rajeev", "Singh", "rajeev@callicoder.com",
-				"MY_SUPER_SECRET_PASSWORD");
-
-		Calendar dateOfBirth = Calendar.getInstance();
-		dateOfBirth.set(1992, 7, 21);
-
-		// Create a UserProfile instance
-		UserProfile userProfile = new UserProfile("+91-8197882053", Gender.MALE, dateOfBirth.getTime(),
-				"747", "2nd Cross", "Golf View Road, Kodihalli", "Bangalore",
-				"Karnataka", "India", "560008");
-
-
-		// Set child reference(userProfile) in parent entity(user)
-		user.setUserProfile(userProfile);
-
-		// Set parent reference(user) in child entity(userProfile)
-		userProfile.setUser(user);
-
-		// Save Parent Reference (which will save the child as well)
-		userRepository.save(user);
-
-		//=========================================
+	@GetMapping("/users")
+	public Collection<User> getUsers(){
+		return userRepository.findAll();
 	}
 
-
+	@GetMapping("/user_profiles")
+	public Collection<UserProfile> getUserProfiles(){
+		return userProfileRepository.findAll();
+	}
 }
---
+++ jpa-one-to-one-demo/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO USERS VALUES (1, 'email1@example.com', 'firstname1', 'lastname1', 'password1'),
+                         (2, 'email2@example.com', 'firstname1', 'lastname1', 'password1'),
+                         (3, 'email3@example.com', 'firstname1', 'lastname1', 'password1');
+
+--ID  	ADDRESS1  	ADDRESS2  	CITY  	COUNTRY  	DOB  	GENDER  	PHONE_NUMBER  	STATE  	STREET  	ZIP_CODE  	USER_ID
+INSERT INTO USER_PROFILES VALUES (1, 'address 1', 'Address 1', 'City 1', 'Country 1', '1995-05-05', 'MALE', 'Phone Number 1', 'State 1', 'Street 1', '12545', 1),
+                                 (2, 'address 2', 'Address 2', 'City 2', 'Country 2', '1995-02-02', 'FEMALE', 'Phone Number 2', 'State 2', 'Street 2', '12545', 2),
+                                 (3, 'address 3', 'Address 3', 'City 3', 'Country 3', '1995-03-03', 'MALE', 'Phone Number 3', 'State 3', 'Street 3', '12545', 3);
